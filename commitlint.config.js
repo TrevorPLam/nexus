41344976dc37819d7084165d35ba390cbbf70f9f
Trevor Lam <thetrevorlam@gmail.com>
2026-07-22 01:02:41 -0500

    chore: update commitlint config to support ticket IDs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?:(?:[A-Z]+-\d+):\s)?(\w+):\s(.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
 };
