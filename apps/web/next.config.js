6c4f21699df9a2ec70078827a72afe039a1f8b66
Trevor Lam <thetrevorlam@gmail.com>
2026-07-23 04:58:19 -0500

    feat: add transpilePackages to Next.js configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@life-os/ui', '@life-os/contracts', '@life-os/api-client'],
 };
 
 module.exports = nextConfig;
